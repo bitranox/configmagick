8713154b00334df9cceb9bdad0ce4ac5655bd20e
bitranox <rnowotny1966@gmail.com>
2019-08-27 20:26:17 +0200

    refractor

--- configmagick/__init__.py
+++ configmagick/__init__.py
@@ -1,10 +1,19 @@
-__title__ = 'configmagick'
-__version__ = '0.0.1'
-__name__ = 'configmagick'
-
 import errno
 import sys
 import logging
+import pathlib
+
+
+def get_version() -> str:
+    with open(pathlib.Path(__file__).parent / 'version.txt', mode='r') as version_file:
+        version = version_file.readline()
+    return version
+
+
+__title__ = 'configmagick'
+__version__ = get_version()
+__name__ = 'configmagick'
+
 # from .libs.lib_classes import RstFile
 # from .libs.lib_classes import RstConf
 # from .libs.lib_main import rst_str_replace
